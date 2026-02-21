9325a1ef31ccc41bdabb9ad2639874092f29a6c9
Laurent AMPLIS <lamplis@yahoo.fr>
2026-02-22 00:03:33 +0100

    build: use androidMultiplatformLibrary and kotlin androidLibrary block

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,18 +2,21 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.androidMultiplatformLibrary)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
 }
 
 kotlin {
-    androidTarget {
+    androidLibrary {
+        namespace = "com.amp_digital.capsule.navigation"
+        compileSdk = libs.versions.android.compileSdk.get().toInt()
+        minSdk = libs.versions.android.minSdk.get().toInt()
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_11)
         }
     }
-    
+
     listOf(
         iosArm64(),
         iosSimulatorArm64()
@@ -46,17 +49,5 @@ kotlin {
             // Ensure Android target sees KMP-Capsule expect/actuals
             implementation(project(":libs:KMP-Capsule"))
         }
-    }
-}
-
-android {
-    namespace = "com.amp_digital.capsule.navigation"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-    defaultConfig {
-        minSdk = libs.versions.android.minSdk.get().toInt()
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
     }
 }
